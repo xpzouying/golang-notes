cdc77f41569bc7b956ce933d9b2755d42cff534c
zouying <xpzouying@gmail.com>
2020-09-03 17:28:59 +0800

    unsafe.Pointer：更新访问结构体的方法

--- docs/unsafe_pointer/README.md
+++ docs/unsafe_pointer/README.md
@@ -1,7 +1,27 @@
 # unsafe.Pointer 
 
+## 通用访问Tips
+
+```go
+// equivalent to f := unsafe.Pointer(&s.f)
+f := unsafe.Pointer(uintptr(unsafe.Pointer(&s)) + unsafe.Offsetof(s.f))
+
+// equivalent to e := unsafe.Pointer(&x[i])
+e := unsafe.Pointer(uintptr(unsafe.Pointer(&x[0])) + i*unsafe.Sizeof(x[0]))
+```
+
 
 ## `unsafe.Pointer`访问数组
 
 当前有个`[]int`数组对象，如何通过`unsafe.Pointer`直接访问对象中的元素。
+
+
+
+## `unsafe.Pointer`访问struct的元素
+
+可以通过偏移访问struct的元素。
+
+```go
+p = unsafe.Pointer(uintptr(p) + offset)
+```
 
--- docs/unsafe_pointer/unsafe_pointer.go
+++ docs/unsafe_pointer/unsafe_pointer.go
@@ -1,6 +1,9 @@
 package main
 
-import "unsafe"
+import (
+	"log"
+	"unsafe"
+)
 
 func accessSlice() {
 	// init slice
@@ -17,6 +20,37 @@ func accessSlice() {
 	}
 }
 
+func accessStruct() {
+	type Friends struct {
+		Name string
+		Age  int
+	}
+
+	name := "zy"
+	age := 18
+	f1 := Friends{Name: name, Age: age}
+
+	log.Printf("sizeof(Friends): %d, sizeof(string)=%d, sizeof(int)=%d", unsafe.Sizeof(f1), unsafe.Sizeof(name), unsafe.Sizeof(age))
+
+	log.Printf("before update f1: %+v", f1)
+
+	// 指针指向f1对象
+	f1Ptr := unsafe.Pointer(&f1)
+
+	// 方法1： f1对象的首地址 + name的大小，等于Age元素
+	agePtr := unsafe.Pointer((uintptr(f1Ptr) + unsafe.Sizeof(name)))
+	*(*int)(agePtr) = 20
+	log.Printf("after update f1: %+v", f1)
+
+	// 方法2：使用unsafe.Offsetof 访问Age元素
+	agePtr = unsafe.Pointer((uintptr(f1Ptr) + unsafe.Offsetof(f1.Age)))
+	*(*int)(agePtr) = 30
+	log.Printf("after update f1: %+v", f1)
+
+}
+
 func main() {
 	accessSlice()
+
+	accessStruct()
 }
