a7a5f9d3966ad752e1c4bfacf58ed2257249dd69
zy <xpzouying@gmail.com>
2020-11-17 23:29:10 +0800

    common_errors: 添加常见错误

--- docs/common_errors/main.go
+++ docs/common_errors/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "log"
+import (
+	"log"
+)
 
 func makeSlice() {
 	l := make([]int, 10)
@@ -11,6 +13,22 @@ func makeSlice() {
 	log.Printf("%v", l)
 }
 
+// 看似一道简单的 Go 题，考点不少：一道字节跳动面试题解析
+// https://mp.weixin.qq.com/s/gQcCzrP8Pvr9WKONOl0ZHQ
+func forGoroutine() {
+	total, sum := 0, 0
+	for i := 1; i <= 10; i++ {
+		sum += i
+		go func() {
+			total += i
+		}()
+	}
+
+	log.Printf("total:%d sum %d", total, sum)
+}
+
 func main() {
-	makeSlice()
+	// makeSlice()
+
+	forGoroutine()
 }
