b368ea7fa91d51ff0dc81fad800b8416cf83b821
zy <xpzouying@gmail.com>
2020-12-20 13:22:03 +0800

    git-bitsect: v4, fix testcase

--- docs/git_bisect/main_test.go
+++ docs/git_bisect/main_test.go
@@ -4,13 +4,19 @@ import "testing"
 
 func TestMod(t *testing.T) {
 
-	got := Mod(5, 3)
+	got, err := Mod(5, 3)
+	if err != nil {
+		t.Error(err)
+	}
 	want := 2
 	if got != want {
 		t.Errorf("expected error")
 	}
 
-	got2 := Mod(5, 2)
+	got2, err := Mod(5, 2)
+	if err != nil {
+		t.Error(err)
+	}
 	want2 := 1
 	if got2 != want2 {
 		t.Error("expected error")
