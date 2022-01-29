140eab0acd25964f2cf14d73f2f86926bd9750ed
zy <xpzouying@gmail.com>
2022-01-29 07:18:39 +0000

    query object: add beginning of query object
    1. add day 1 example: init the user case project
    2. add README.md for introduce the projecrt

---
+++ query_object_pattern/day1_user_app/biz/user.go
@@ -0,0 +1,42 @@
+package biz
+
+import (
+	"context"
+	"errors"
+)
+
+type User struct {
+	ID   int `gorm:"primaryKey"`
+	Name string
+	Age  int
+}
+
+type UserRepo interface {
+	SaveUser(ctx context.Context, user *User) (*User, error)
+	GetUserByID(ctx context.Context, id int) (*User, error)
+}
+
+type UserUsecase struct {
+	userRepo UserRepo
+}
+
+func NewUserUsecase(userRepo UserRepo) (*UserUsecase, error) {
+	if userRepo == nil {
+		return nil, errors.New("user repository is nil")
+	}
+
+	return &UserUsecase{userRepo}, nil
+}
+
+func (uc *UserUsecase) SaveUser(ctx context.Context, name string, age int) (*User, error) {
+
+	return uc.userRepo.SaveUser(ctx, &User{
+		Name: name,
+		Age:  age,
+	})
+}
+
+func (uc *UserUsecase) GetUserByID(ctx context.Context, id int) (*User, error) {
+
+	return uc.userRepo.GetUserByID(ctx, id)
+}
---
+++ query_object_pattern/day1_user_app/data/user_repo.go
@@ -0,0 +1,76 @@
+package data
+
+import (
+	"context"
+	"errors"
+
+	"user_app/biz"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID   int
+	Name string
+	Age  uint8
+
+	gorm.Model
+}
+
+func (User) Tablename() string {
+	return "user"
+}
+
+type UserRepo struct {
+	db *gorm.DB
+}
+
+func NewMemSqlite() (*gorm.DB, error) {
+	return gorm.Open(
+		sqlite.Open("file::memory:?cache=shared"),
+		&gorm.Config{},
+	)
+}
+
+func NewUserRepo(db *gorm.DB) (*User, error) {
+
+	if db == nil {
+		return nil, errors.New("connection is nil")
+	}
+
+	// Auto Migrate
+	db.AutoMigrate(&User{})
+
+	return &UserRepo{db}, nil
+}
+
+func (us *UserRepo) SaveUser(ctx context.Context, user *biz.User) (*biz.User, error) {
+	u := User{
+		Name: user.Name,
+		Age:  uint8(user.Age),
+	}
+	if err := us.db.Create(&u).Error; err != nil {
+		return nil, err
+	}
+
+	return &biz.User{
+		ID:   u.ID,
+		Name: u.Name,
+		Age:  int(u.Age),
+	}, nil
+}
+
+func (us *UserRepo) GetUserByID(ctx context.Context, uid int) (*biz.User, error) {
+
+	var user User
+	if err := us.db.Where("id = ?", uid).Take(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &biz.User{
+		ID:   user.ID,
+		Age:  int(user.Age),
+		Name: user.Name,
+	}, nil
+}
